3ddd5d62dc38eebc5717f10c54acaa28eedd65e6
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-09-06 04:32:02 +1000

    Updating the Serverless request handler

--- serverless/src/main/scala/com/ruchij/serverless/handlers/ApiGatewayRequestHandler.scala
+++ serverless/src/main/scala/com/ruchij/serverless/handlers/ApiGatewayRequestHandler.scala
@@ -3,10 +3,11 @@ package com.ruchij.serverless.handlers
 import cats.effect.{Clock, IO}
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.ruchij.api.config.ServiceConfiguration
 import com.ruchij.api.services.health.HealthServiceImpl
 import com.ruchij.api.web.Routes
 import com.ruchij.serverless.Transformers
-import org.http4s.Request
+import pureconfig.ConfigSource
 
 class ApiGatewayRequestHandler extends RequestHandler[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] {
 
@@ -14,14 +15,15 @@ class ApiGatewayRequestHandler extends RequestHandler[APIGatewayProxyRequestEven
     apiGatewayProxyRequestEvent: APIGatewayProxyRequestEvent,
     context: Context
   ): APIGatewayProxyResponseEvent = {
-    val http4sRequest: IO[Request[IO]] = Transformers.requestDecoder[IO](apiGatewayProxyRequestEvent)
     implicit val clock: Clock[IO] = Clock.create[IO]
-
-    val healthService = new HealthServiceImpl[IO]
 
     val result =
       for {
-        request <- http4sRequest
+        configObjectSource <- IO.delay(ConfigSource.defaultApplication)
+        serviceConfiguration <- ServiceConfiguration.parse[IO](configObjectSource)
+        healthService = new HealthServiceImpl[IO](serviceConfiguration.buildInformation)
+
+        request <- Transformers.requestDecoder[IO](apiGatewayProxyRequestEvent)
         http4sResponse <- Routes(healthService).run(request)
 
         response <- Transformers.responseEncoder(http4sResponse)
