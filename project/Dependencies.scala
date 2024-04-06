90fe9b227310047bd44f647658c940400380e151
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-04-07 01:31:21 +1100

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies
   val ScalaVersion = "2.13.13"
   val Http4sVersion = "0.23.26"
   val CirceVersion = "0.14.6"
-  val AwsSdkVersion = "2.25.20"
+  val AwsSdkVersion = "2.25.26"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
 
@@ -33,7 +33,7 @@ object Dependencies
 
   lazy val awsSns = "software.amazon.awssdk" % "sns" % AwsSdkVersion
 
-  lazy val phoneNumber = "com.googlecode.libphonenumber" % "libphonenumber" % "8.13.33"
+  lazy val phoneNumber = "com.googlecode.libphonenumber" % "libphonenumber" % "8.13.34"
 
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.3"
 
